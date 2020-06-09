9a464b255c394bd24f315504adc241ea2e34f3de
Sohailrazam <6760121m>
2020-06-09 17:21:54 +0500

    update app.js
    changing raza to mehdi

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react by sohail mehdi
+          learn react by sohail raza 
           
         </a>
       </header>
